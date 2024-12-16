7320a475b94a3a116d77d72c71a22cbab28b7d72
Vishal Sharma <sharmavishal1442000@gmail.com>
2024-12-16 18:30:44 +0530

    stack example

--- practice/src/_16_stack/StackImplementation.java
+++ practice/src/_16_stack/StackImplementation.java
@@ -1,7 +1,15 @@
 package _16_stack;
 
+import java.util.Stack;
+
 public class StackImplementation {
     public static void main(String[] args) {
-        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack);
     }
 }
